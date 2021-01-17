88513f8054f307d366b88117697536121a4f8b5a
jiaoyuanyuan <sxnddz13@163.com>
2021-01-17 19:29:08 +0800

    心跳接口提交接口

--- src/main/java/com/hengtianyi/dims/api/LoginApiController.java
+++ src/main/java/com/hengtianyi/dims/api/LoginApiController.java
@@ -2,14 +2,12 @@ package com.hengtianyi.dims.api;
 
 import com.hengtianyi.common.core.base.UserDTO;
 import com.hengtianyi.common.core.constant.BaseConstant;
+import com.hengtianyi.common.core.feature.ServiceResult;
 import com.hengtianyi.dims.service.api.SysUserService;
 import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 登录api
@@ -33,5 +31,17 @@ public class LoginApiController {
   @PostMapping(value = "/login.json", produces = BaseConstant.JSON)
   public String login(@RequestBody UserDTO dto) {
     return sysUserService.appLogin(dto);
+  }
+
+  /**
+   * 心跳测试接口
+   *
+   * @return json+token
+   */
+  @GetMapping(value = "/heart", produces = BaseConstant.JSON)
+  public String heart() {
+    ServiceResult<Object> result = new ServiceResult<>();
+    result.setSuccess(true);
+    return result.toJson();
   }
 }
